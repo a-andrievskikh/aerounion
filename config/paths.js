cc3e1c9470084a93cff2fa61698ae9668f769699
a-andrievskikh <sn0abg0@gmail.com>
2022-09-19 19:37:10 +0500

    delete: webpack config files

--- config/paths.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-	scr: path.resolve(__dirname, '../src'),
-	build: path.resolve(__dirname, '../dist'),
-	public: path.resolve(__dirname, '../public'),
-};
