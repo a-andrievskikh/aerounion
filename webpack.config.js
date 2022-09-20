e375238f754ccd526e020f0071680833a92ec6b7
a-andrievskikh <sn0abg0@gmail.com>
2022-09-20 19:45:21 +0500

    feat: add entry and output

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,23 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+//Исли переменная не определена, то будет режим 'dev'
+const mode = process.env.NODE_ENV || 'development';
+
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : false;
+
+module.exports = {
+	mode,
+	target,
+	devtool,
+	entry: path.resolve(__dirname, 'src', 'index.js'),
+
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[contenthash].bundle.js',
+		clean: true,
+	},
+};
