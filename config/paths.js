32d69f7879f50cff4126bcb53ad89b9b384cca0a
a-andrievskikh <sn0abg0@gmail.com>
2022-09-21 15:43:42 +0500

    feat: set up paths

---
+++ config/paths.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+	src: path.resolve(__dirname, '../src'),
+
+	dev: path.resolve(__dirname, '../build'),
+
+	build: path.resolve(__dirname, '../dist'),
+};
