9c5026fdac073be702b2b3500daa4ad53240f68a
a-andrievskikh <sn0abg0@gmail.com>
2022-09-27 14:14:59 +0500

    del: delete paths.js

--- config/paths.js
+++ config/paths.js
@@ -1,9 +1,8 @@
-const path = require('path');
+
 
 module.exports = {
-	src: path.resolve(__dirname, '../src'),
 
-	dev: path.resolve(__dirname, '../build'),
 
-	build: path.resolve(__dirname, '../dist'),
+
+
 };
