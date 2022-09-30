d6f9fb236af90d18611fd5b2a914170952b7ecbb
a-andrievskikh <sn0abg0@gmail.com>
2022-09-30 18:52:44 +0500

    fix: output path

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -13,7 +13,7 @@ module.exports = {
 	context: path.resolve(__dirname, '../src'),
 	entry: ['@babel/polyfill', '/index.js'],
 	output: {
-		path: path.resolve(__dirname, '../dist'),
+		path: path.resolve(__dirname, 'dist'),
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
