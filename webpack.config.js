4bba3dafc90a0e492df2d57cd36f93c77a9f815b
a-andrievskikh <sn0abg0@gmail.com>
2022-10-17 18:09:04 +0500

    chore: rn entry.index

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
 	devtool,
 	context: path.resolve(__dirname, 'src'),
 	entry: {
-		index: 'template.pug',
+		index: 'index.pug',
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
